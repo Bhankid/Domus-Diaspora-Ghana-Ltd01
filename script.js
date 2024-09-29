c94fcd6ece5bbe01c075e7faa78208e107cc27c6
Bhankid <alfredfianyo50@gmail.com>
2024-09-29 03:35:01 +0000

    Revert "update .

--- script.js
+++ script.js
@@ -28,36 +28,6 @@ document.addEventListener("click", function (event) {
   }
 });
 
-// Get the elements
-const navToggle = document.getElementById('open-nav');
-const navMenu = document.getElementById('nav-links');
-const navDropdowns = document.querySelectorAll('.dropdown');
-
-// Add event listener to the nav toggle
-navToggle.addEventListener('click', () => {
-  // Toggle the nav menu
-  navMenu.classList.toggle('open');
-});
-
-// Add event listener to the dropdown toggles
-navDropdowns.forEach((dropdown) => {
-  const dropdownToggle = dropdown.querySelector('.dropdown-toggle');
-  const dropdownMenu = dropdown.querySelector('.dropdown-menu');
-
-  dropdownToggle.addEventListener('click', () => {
-    // Toggle the dropdown menu
-    dropdownMenu.classList.toggle('open');
-
-    // Add a nice transition effect
-    dropdownMenu.style.transition = 'max-height 0.5s ease-in-out';
-    if (dropdownMenu.classList.contains('open')) {
-      dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + 'px';
-    } else {
-      dropdownMenu.style.maxHeight = '0px';
-    }
-  });
-});
-
 // Card swiper
 const stack = document.querySelector(".stack");
 const cards = Array.from(stack.children)
