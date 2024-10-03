02cfca8683852286581c19fbafc6bba2422c6f4c
Bhankid <alfredfianyo50@gmail.com>
2024-10-03 09:03:52 +0000

    main slider to image

--- script.js
+++ script.js
@@ -82,39 +82,39 @@ autoplay();
 document.addEventListener("click", activate, false);
 
 
-// Card swiper
-const stack = document.querySelector(".stack");
-const cards = Array.from(stack.children)
-  .reverse()
-  .filter((child) => child.classList.contains("card"));
+// // Card swiper
+// const stack = document.querySelector(".stack");
+// const cards = Array.from(stack.children)
+//   .reverse()
+//   .filter((child) => child.classList.contains("card"));
 
-cards.forEach((card) => stack.appendChild(card));
+// cards.forEach((card) => stack.appendChild(card));
 
-function moveCard() {
-  const lastCard = stack.lastElementChild;
-  if (lastCard.classList.contains("card")) {
-    lastCard.classList.add("swap");
+// function moveCard() {
+//   const lastCard = stack.lastElementChild;
+//   if (lastCard.classList.contains("card")) {
+//     lastCard.classList.add("swap");
 
-    setTimeout(() => {
-      lastCard.classList.remove("swap");
-      stack.insertBefore(lastCard, stack.firstElementChild);
-    }, 1200);
-  }
-}
+//     setTimeout(() => {
+//       lastCard.classList.remove("swap");
+//       stack.insertBefore(lastCard, stack.firstElementChild);
+//     }, 1200);
+//   }
+// }
 
-let autoplayInterval = setInterval(moveCard, 4000);
+// let autoplayInterval = setInterval(moveCard, 4000);
 
-stack.addEventListener("click", function (e) {
-  const card = e.target.closest(".card");
-  if (card && card === stack.lastElementChild) {
-    card.classList.add("swap");
+// stack.addEventListener("click", function (e) {
+//   const card = e.target.closest(".card");
+//   if (card && card === stack.lastElementChild) {
+//     card.classList.add("swap");
 
-    setTimeout(() => {
-      card.classList.remove("swap");
-      stack.insertBefore(card, stack.firstElementChild);
-    }, 1200);
-  }
-});
+//     setTimeout(() => {
+//       card.classList.remove("swap");
+//       stack.insertBefore(card, stack.firstElementChild);
+//     }, 1200);
+//   }
+// });
 
 
 
